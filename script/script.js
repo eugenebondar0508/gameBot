0be6f62d46a6b93c45d6505bd8941f4932005d19
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-06-07 15:28:51 +0300

    added additional task

--- script/script.js
+++ script/script.js
@@ -9,27 +9,36 @@ let isNumber = function(n){
 
 let game = function(){
    let correctNumber = Math.floor(Math.random() * 101);
+   let count = 10;
+
    let getNumber = function(){
       let userNumber = prompt('Угадай число от 1 до 100');
-      if(userNumber === null){
+      if(userNumber === null || count === 0){
          alert('Игра окончена');
       } else if (!isNumber(userNumber) || parseFloat(userNumber) > 100){
          confirm('Введи число от 0 до 100!');
+          count--;
         return getNumber();
       } else if
       (userNumber == correctNumber){
          alert('Ты угадал!!!');
       } else if (userNumber > correctNumber){
-         confirm('Загаданное число меньше. Попробую еще раз');
+         confirm('Загаданное число меньше. Попробую еще раз. Осталось ' +count+ ' попыток');
+       count--;
           return getNumber();
       } else if (userNumber < correctNumber){
-         confirm('Загаданное число больше. Попробую еще раз');
-         return getNumber();
+         confirm('Загаданное число больше. Попробую еще раз. Осталось ' +count+ ' попыток');
+        count--;
+         return getNumber() ;
+         
       } 
-   
+      
    }
+  console.log(count);
    return getNumber();
-}
+
+}  
+
 game();
 
 
