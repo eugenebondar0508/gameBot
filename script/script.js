a7e733a73b107ff0a77618411679bed2bc82f887
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-06-07 16:31:03 +0300

    fixed bugs

--- script/script.js
+++ script/script.js
@@ -9,19 +9,33 @@ let isNumber = function(n){
 
 let game = function(){
    let correctNumber = Math.floor(Math.random() * 101);
-   let count = 10;
+   let count = 9;
 
    let getNumber = function(){
       let userNumber = prompt('Угадай число от 1 до 100');
-      if(userNumber === null || count === 0){
-         alert('Игра окончена');
+      if(userNumber === null){
+        alert('Игра окончена');
       } else if (!isNumber(userNumber) || parseFloat(userNumber) > 100){
          confirm('Введи число от 0 до 100!');
           count--;
-        return getNumber();
-      } else if
+          return getNumber(); 
+       
+      }else if(count === 0){
+         if(confirm('Попытки закончились, хотите сыграть еще?') === true){
+            count=9;
+            return getNumber();
+         } else{
+            return alert('Игра окончена');
+         }
+      } 
+      else if
       (userNumber == correctNumber){
-         alert('Ты угадал!!!');
+         if(confirm('Ты угадал!!! Хотели бы сыграть еще?') ===false){
+            return alert('Игра окончена');
+         } else{
+            count=9;
+            return getNumber();
+         }
       } else if (userNumber > correctNumber){
          confirm('Загаданное число меньше. Попробую еще раз. Осталось ' +count+ ' попыток');
        count--;
